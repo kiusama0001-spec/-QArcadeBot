779723a88b1adb2548cfc27a3806b58f7be47d40
kiusama0001-spec <kiusama0001@gmail.com>
2026-09-16 14:33:39 -0600

    Change default port to 10000 in run_web.py

--- run_web.py
+++ run_web.py
@@ -3,10 +3,6 @@ import uvicorn
 from webapp.app import app
 
 if __name__ == "__main__":
-    # Render asigna un puerto dinámico mediante la variable de entorno PORT.
-    # Si no existe (por ejemplo, si lo pruebas en tu computadora), usará el puerto 8000 por defecto.
-    port = int(os.environ.get("PORT", 8000))
-    
-    # Arranca la aplicación escuchando en todas las interfaces (0.0.0.0) y en el puerto correcto de Render
+    port = int(os.environ.get("PORT", 10000))
     uvicorn.run(app, host="0.0.0.0", port=port
                )
